b7661ab980bb2f9d73b0fe2e0848ede17d6f1d10
Prithviraj97 <prithvirajsingh358@gmail.com>
2024-04-23 22:20:09 -0500

    formatting fixed

--- ML_application/app.py
+++ ML_application/app.py
@@ -1,19 +1,4 @@
-# from flask import Flask, render_template
-
-# app = Flask(__name__)
-
-# @app.route('/')
-# def index():
-#     # Fetch data from the database and prepare for rendering.
-#     data = {'title': 'My Flask App'}
-
-#     # Render the HTML template.
-#     return render_template('ML_application\check.html', data=data)
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-# from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import cv2
 import numpy as np
 
@@ -46,8 +31,6 @@ import numpy as np
 
 # if __name__ == '__main__':
 #     app.run(debug=True)
-
-from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
